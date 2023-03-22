ce3d73c543dbeb36e4f1082fb9794316b85e7d2c
nithin monni <nithin.monni@gmail.com>
2023-03-22 11:17:04 -0500

    created express app to save download and save song

---
+++ download/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { Spotify } from "spotifydl-core";
+import fs from "fs";
+import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
+import ffmpeg from "fluent-ffmpeg";
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+const credentials = {
+  clientId: "bf48614d72f24549881ec3aca5ac064c",
+  clientSecret: "6064b32d56a24b39a74fe36e9a4a5866",
+};
+const spotify = new Spotify(credentials);
+
+const download = async (trackId) => {
+  const song = await spotify.downloadTrack(trackId); // Downloading goes brr brr
+  fs.writeFileSync(`../songs/${trackId}.mp3`, song);
+};
+
+const app = express();
+
+app.all("/:id", async (req, res) => {
+  try {
+    await download(req.params.id);
+  } catch {
+    console.log("weird err");
+  } finally {
+    res.send("OK");
+  }
+});
+
+app.listen(9999, () => {
+  console.log("download app running on 9999");
+});
