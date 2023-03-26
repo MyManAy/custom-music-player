26ccab8cad7f859f1cb5d7a1136c81a45cf8e0b6
nithin monni <nithin.monni@gmail.com>
2023-03-25 23:41:22 -0500

    updated eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -7,13 +7,13 @@ const config = {
       ],
       files: ["*.ts", "*.tsx"],
       parserOptions: {
-        project: "tsconfig.json",
+        project: "./client/tsconfig.json",
       },
     },
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "./tsconfig.json",
+    project: "./client/tsconfig.json",
   },
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
