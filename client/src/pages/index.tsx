69a4bbe13788623ff2f59d2b0ad4f3beb4b9f934
MyManAy <66086069+MyManAy@users.noreply.github.com>
2023-03-29 23:57:50 -0500

    fixed infinite login redirect bug

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -8,21 +8,22 @@ import { getAccessToken } from "~/utils/spotify";
 
 type BooleanString = "true" | "false" | undefined | "";
 
-const myFunc = async (authTimedOut: BooleanString) => {
+const myFunc = async (authTimedOut: BooleanString, accessToken: string) => {
   const token = await getAccessToken(window, Boolean(authTimedOut));
-  console.log(token);
-  if (token) {
-    router.push(`/home/${token}`).catch(() => console.log("didn't work"));
-  }
+  if (token)
+    router
+      .push(`/home/${accessToken === token ? accessToken : token}`)
+      .catch(() => console.log("didn't work"));
 };
 
 const PreLogin = () => {
   const router = useRouter();
   const [port, setPort] = useState(null as number | null);
   const authTimedOut = router.query["authTimedOut"] as BooleanString;
+  const accessToken = router.query["access_token"] as string;
 
   useEffect(() => {
-    myFunc(authTimedOut).catch(() => console.log("uh oh"));
+    myFunc(authTimedOut, accessToken).catch(() => console.log("uh oh"));
     setPort(Number(window.location.port));
   }, []);
 
